fccc9a9d98169d1ea59e2085aeac690e67aeb220
Angie Vanegas <arco527@gmail.com>
2017-04-23 22:51:43 -0500

    Add, remove

--- js/main.js
+++ js/main.js
@@ -1,151 +1,59 @@
 (function(){
-	// variables
-
+	// Variables
 	var lista = document.getElementById("lista"),
-			tareaInput = document.getElementById("tareaInput"),
-			btn = document.getElementById("btn-agregar");
+		tareaInput = document.getElementById("tareaInput"),
+		btnNuevaTarea = document.getElementById("btn-agregar");
 
+	// Funciones
+	var agregarTarea = function(){
+		var tarea = tareaInput.value,
+			nuevaTarea = document.createElement("li"),
+			enlace = document.createElement("a"),
+			contenido = document.createTextNode(tarea);
 
-			// funciones
-			var agregarTarea = function(){
-				alert("Agregar Tarea")};
-			var comprobarInput = function(){
-				alert("Comprobar input")};
-			var eliminarTarea = function(){
-				alert("Eliminar tarea")};
+		if (tarea === "") {
+			tareaInput.setAttribute("placeholder", "Agrega una tarea valida");
+			tareaInput.className = "error";
+			return false;
+		}
 
+		// Agregamos el contenido al enlace
+		enlace.appendChild(contenido);
+		// Le establecemos un atributo href
+		enlace.setAttribute("href", "#");
+		// Agrergamos el enlace (a) a la nueva tarea (li)
+		nuevaTarea.appendChild(enlace);
+		// Agregamos la nueva tarea a la lista
+		lista.appendChild(nuevaTarea);
 
+		tareaInput.value = "";
 
-			// eventos
-			  //agregar tarea
-			btnNuevaTarea = addEventListener("click", agregarTarea);  // sin () aunque es una función
+		for (var i = 0; i <= lista.children.length -1; i++) {
+			lista.children[i].addEventListener("click", function(){
+				this.parentNode.removeChild(this);
+			});
+		}
 
-			//Comprobar input
-			tareaInput = addEventListener("click", comprobarInput);
+	};
+	var comprobarInput = function(){
+		tareaInput.className = "";
+		teareaInput.setAttribute("placeholder", "Agrega tu tarea");
+	};
 
-			//Borrando elementos de la lista
-			for (var i = 0; i < lista.children.length -1; i++) {
-				lista.children[i].addEventListener("click", eliminarTarea);
-			}
+	var eleminarTarea = function(){
+		this.parentNode.removeChild(this);
+	};
 
+	// Eventos
 
+	// Agregar Tarea
+	btnNuevaTarea.addEventListener("click", agregarTarea);
+
+	// Comprobar Input
+	tareaInput.addEventListener("click", comprobarInput);
+
+	// Borrando Elementos de la lista
+	for (var i = 0; i <= lista.children.length -1; i++) {
+		lista.children[i].addEventListener("click", eleminarTarea);
+	}
 }());
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// (function(){
-// 	// Variables
-// 	var lista = document.getElementById("lista"),
-// 		tareaInput = document.getElementById("tareaInput"),
-// 		btnNuevaTarea = document.getElementById("btn-agregar");
-//
-// 	// Funciones
-// 	var agregarTarea = function(){
-// 		var tarea = tareaInput.value,
-// 			nuevaTarea = document.createElement("li"),
-// 			enlace = document.createElement("a"),
-// 			contenido = document.createTextNode(tarea);
-//
-// 		if (tarea === "") {
-// 			tareaInput.setAttribute("placeholder", "Agrega una tarea valida");
-// 			tareaInput.className = "error";
-// 			return false;
-// 		}
-//
-// 		// Agregamos el contenido al enlace
-// 		enlace.appendChild(contenido);
-// 		// Le establecemos un atributo href
-// 		enlace.setAttribute("href", "#");
-// 		// Agrergamos el enlace (a) a la nueva tarea (li)
-// 		nuevaTarea.appendChild(enlace);
-// 		// Agregamos la nueva tarea a la lista
-// 		lista.appendChild(nuevaTarea);
-//
-// 		tareaInput.value = "";
-//
-// 		for (var i = 0; i <= lista.children.length -1; i++) {
-// 			lista.children[i].addEventListener("click", function(){
-// 				this.parentNode.removeChild(this);
-// 			});
-// 		}
-//
-// 	};
-// 	var comprobarInput = function(){
-// 		tareaInput.className = "";
-// 		teareaInput.setAttribute("placeholder", "Agrega tu tarea");
-// 	};
-//
-// 	var eleminarTarea = function(){
-// 		this.parentNode.removeChild(this);
-// 	};
-//
-// 	// Eventos
-//
-// 	// Agregar Tarea
-// 	btnNuevaTarea.addEventListener("click", agregarTarea);
-//
-// 	// Comprobar Input
-// 	tareaInput.addEventListener("click", comprobarInput);
-//
-// 	// Borrando Elementos de la lista
-// 	for (var i = 0; i <= lista.children.length -1; i++) {
-// 		lista.children[i].addEventListener("click", eleminarTarea);
-// 	}
-// }());
